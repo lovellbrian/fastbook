aa1dd3eb3d6b9ba5782464e9347d86ec2d05256e
Brian Lovell <lovell.brian@gmail.com>
2024-05-03 03:38:05 +0000

    Added gpufrozen branch

--- .devcontainer/install-dev-tools.sh
+++ .devcontainer/install-dev-tools.sh
@@ -1,6 +1,32 @@
 # Put any custom installs in this file
-# pip3 cache purge
-# sudo apt-get autoremove -y
-# sudo apt-get clean
 
-pip install -Uqq fastbook
+# Put files in USER_FOLDER. Only do this once. 
+
+echo -e "Creating the /home/vscode/.local python respository\n" 
+
+USER_FOLDER="/home/vscode/.local"
+if [ ! -d "$USER_FOLDER" ]; then
+ 
+  # install python code to ~/.vscode/.local
+  pip install --upgrade pip
+  sudo apt-get install -y libcairo2-dev pkg-config python3-dev
+  pip install -r .devcontainer/requirements.txt
+
+  # Let's have a user version of python3.
+  cp /usr/local/bin/python3.10 /home/vscode/.local/bin
+  ln -s /home/vscode/.local/bin/python3.10  /home/vscode/.local/bin/python3
+
+  # Version 8 needed for RISE slides. Generates a red compatibility error. 
+  pip install -U ipywidgets==8.0.0
+
+  # Put extra packages here
+
+fi
+
+
+# Notes:
+# Commands for presentation using jupyter notebook
+# cd slides
+# jupyter nbconvert birds.ipynb --to slides --post serve
+# Jupyter notebook
+
