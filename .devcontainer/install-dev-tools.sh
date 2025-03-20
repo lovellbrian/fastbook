133358f368f53b8a04f37a7d5671e158e7a5726c
Brian Lovell <lovell.brian@gmail.com>
2025-03-20 13:07:36 +1000

    Added cpu branch

--- .devcontainer/install-dev-tools.sh
+++ .devcontainer/install-dev-tools.sh
@@ -1,26 +1,23 @@
+#!/bin/bash
+
 # Put any custom installs in this file
 
 # Put files in USER_FOLDER. Only do this once. 
-
-echo -e "Creating the /home/vscode/.local python respository\n" 
-
 USER_FOLDER="/home/vscode/.local"
 if [ ! -d "$USER_FOLDER" ]; then
- 
+  echo -e "Creating the $USER_FOLDER python respository\n" 
   # install python code to ~/.vscode/.local
   pip install --upgrade pip
-  sudo apt-get install -y libcairo2-dev pkg-config python3-dev
   pip install -r .devcontainer/requirements.txt
-
   # Let's have a user version of python3.
   cp /usr/local/bin/python3.10 /home/vscode/.local/bin
-  ln -s /home/vscode/.local/bin/python3.10  /home/vscode/.local/bin/python3
-
+  ln -s /home/vscode/.local/bin/python3.10 /home/vscode/.local/bin/python3
   # Version 8 needed for RISE slides. Generates a red compatibility error. 
   pip install -U ipywidgets==8.0.0
-
   # Put extra packages here
 
+else
+  echo -e "User folder $USER_FOLDER already exists\n"
 fi
 
 
