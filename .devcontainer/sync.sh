8fd1019940a9b1437676c17c40669df7478bface
Brian Lovell <lovell.brian@gmail.com>
2025-12-12 01:44:37 +0000

    Updated

--- .devcontainer/sync.sh
+++ .devcontainer/sync.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-# This bash shell script allows us the synchronize specified files from the cpufrozen branch to the cpu, gpu, and gpu frozen branches
+# This bash shell script allows us the synchronize specified files from the gpufrozen branch to the cpu, gpu, and cpufrozen frozen branches
 
-# Usage: ./sync.sh <file1> <file2> ... <fileN>
+# Usage: .devcontainer/sync.sh <file1> <file2> ... <fileN>
 git stash
 sleep 1
 git checkout cpufrozen && yes | git checkout --patch gpufrozen -- $@
+sleep 1
+git add $@ && git commit -m "Sync" && git push
+git checkout cpu && yes | git checkout --patch gpufrozen -- $@
+sleep 1
+git add $@ && git commit -m "Sync" && git push
+git checkout gpu && yes | git checkout --patch gpufrozen -- $@
 sleep 1
 git add $@ && git commit -m "Sync" && git push
 sleep 1
