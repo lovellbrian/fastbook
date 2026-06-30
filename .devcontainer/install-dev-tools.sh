39746da209acd489b644bf6ca47960450478908b
Brian Lovell <lovell.brian@gmail.com>
2026-06-30 06:40:22 +0000

    Revert to 11.8 Cuda

--- .devcontainer/install-dev-tools.sh
+++ .devcontainer/install-dev-tools.sh
@@ -13,8 +13,8 @@ if [ ! -d "$USER_FOLDER" ]; then
   pip install -r .devcontainer/requirements.txt
 
   # Let's have a user version of python3.
-  cp /usr/local/bin/python3.10 /home/vscode/.local/bin
-  ln -s /home/vscode/.local/bin/python3.10  /home/vscode/.local/bin/python3
+  # cp /usr/local/bin/python3.10 /home/vscode/.local/bin
+  # ln -s /home/vscode/.local/bin/python3.10  /home/vscode/.local/bin/python3
 
   # Version 8 needed for RISE slides. Generates a red compatibility error. 
   # pip install -U ipywidgets==7.8.0
