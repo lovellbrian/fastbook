c0fc1cbbadc007ad7c7e8057781cff662f845a53
Brian Lovell <lovell.brian@gmail.com>
2026-07-01 04:36:54 +0000

    Updated Software

--- .devcontainer/install-dev-tools.sh
+++ .devcontainer/install-dev-tools.sh
@@ -2,15 +2,20 @@
 
 # Put files in USER_FOLDER. Only do this once. 
 
-echo -e "Creating the /home/vscode/.local python respository\n" 
+echo
+echo -e "*******************************************************\n"
+echo -e "Creating the /home/vscode/.local python respository\n"
+echo -e "*******************************************************\n" 
+echo 
 
 USER_FOLDER="/home/vscode/.local"
 if [ ! -d "$USER_FOLDER" ]; then
  
-  # install python code to ~/.vscode/.local
+  echo
+  install python code to ~/.vscode/.local
   pip install --upgrade pip
-  sudo apt-get install -y libcairo2-dev pkg-config python3-dev
-  pip install -r .devcontainer/requirements.txt
+  # sudo apt-get install -y libcairo2-dev 
+  pip install --break-system-packages -r .devcontainer/requirements.txt
 
   # Let's have a user version of python3.
   # cp /usr/local/bin/python3.10 /home/vscode/.local/bin
