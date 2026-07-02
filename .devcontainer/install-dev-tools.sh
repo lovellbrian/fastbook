93d76004903f1e7ded589bf73c57814107be04c0
Brian Lovell <lovell.brian@gmail.com>
2026-07-02 03:23:26 +0000

    Builds but no kernel

--- .devcontainer/install-dev-tools.sh
+++ .devcontainer/install-dev-tools.sh
@@ -9,17 +9,20 @@ echo -e "*******************************************************\n"
 echo 
 
 USER_FOLDER="/home/vscode/.local"
-if [ ! -d "$USER_FOLDER" ]; then
- 
-  echo
-  install python code to ~/.vscode/.local
+
+if [  -d "$USER_FOLDER" ]; then
+  # install python code to ~/.vscode/.local
+  echo " got to here"
   pip install --upgrade pip
-  # sudo apt-get install -y libcairo2-dev 
+  sudo apt-get update
+  sudo apt-get install -y cmake
+  sudo apt-get install -y libcairo2-dev 
+  sudo apt-get install -y libgirepository1.0-dev
   pip install --break-system-packages -r .devcontainer/requirements.txt
 
   # Let's have a user version of python3.
-  # cp /usr/local/bin/python3.10 /home/vscode/.local/bin
-  # ln -s /home/vscode/.local/bin/python3.10  /home/vscode/.local/bin/python3
+  cp /usr/local/bin/python3.10 /home/vscode/.local/bin
+  ln -s /home/vscode/.local/bin/python3.10  /home/vscode/.local/bin/python3
 
   # Version 8 needed for RISE slides. Generates a red compatibility error. 
   # pip install -U ipywidgets==7.8.0
