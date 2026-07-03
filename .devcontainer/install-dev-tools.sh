c23425b5a016f00ed39721736a720bac382cbfc1
Brian Lovell <lovell.brian@gmail.com>
2026-07-03 02:02:05 +0000

    Fixed

--- .devcontainer/install-dev-tools.sh
+++ .devcontainer/install-dev-tools.sh
@@ -2,27 +2,22 @@
 
 # Put files in USER_FOLDER. Only do this once. 
 
-echo
-echo -e "*******************************************************\n"
-echo -e "Creating the /home/vscode/.local python respository\n"
-echo -e "*******************************************************\n" 
-echo 
+echo -e "Creating the /home/vscode/.local python respository\n" 
 
 USER_FOLDER="/home/vscode/.local"
 if [ ! -d "$USER_FOLDER" ]; then
  
-  echo
-  install python code to ~/.vscode/.local
+  # install python code to ~/.vscode/.local
   pip install --upgrade pip
-  # sudo apt-get install -y libcairo2-dev 
-  pip install --break-system-packages -r .devcontainer/requirements.txt
+  sudo apt-get install -y libcairo2-dev pkg-config python3-dev
+  pip install -r .devcontainer/requirements.txt
 
   # Let's have a user version of python3.
-  # cp /usr/local/bin/python3.10 /home/vscode/.local/bin
-  # ln -s /home/vscode/.local/bin/python3.10  /home/vscode/.local/bin/python3
+  cp /usr/local/bin/python3.10 /home/vscode/.local/bin
+  ln -s /home/vscode/.local/bin/python3.10  /home/vscode/.local/bin/python3
 
   # Version 8 needed for RISE slides. Generates a red compatibility error. 
-  # pip install -U ipywidgets==7.8.0
+  # pip install -U ipywidgets==8.0.0
 
   # Put extra packages here
 
@@ -34,4 +29,3 @@ fi
 # cd slides
 # jupyter nbconvert birds.ipynb --to slides --post serve
 # Jupyter notebook
-
