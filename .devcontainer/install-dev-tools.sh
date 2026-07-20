2b1d44766c90c404d32c7064789e971afbf9a7e4
Brian Lovell <lovell.brian@gmail.com>
2026-07-20 02:58:43 +0000

    Update shebang and comment out unused code in install-dev-tools.sh

--- .devcontainer/install-dev-tools.sh
+++ .devcontainer/install-dev-tools.sh
@@ -1,29 +1,29 @@
-#!/bin/bash
+#!/bin/env bash
 
 # Put any custom installs in this file
 
 # Put files in USER_FOLDER. Only do this once. 
 
-echo -e "Creating the /home/vscode/.local python respository\n" 
+# echo -e "Creating the /home/vscode/.local python respository\n" 
 
-USER_FOLDER="/home/vscode/.local"
-if [ ! -d "$USER_FOLDER" ]; then
+# USER_FOLDER="/home/vscode/.local"
+# if [ ! -d "$USER_FOLDER" ]; then
  
   # install python code to ~/.vscode/.local
   pip install --upgrade pip
-  sudo apt-get install -y libcairo2-dev pkg-config python3-dev
+  # sudo apt-get install -y libcairo2-dev pkg-config python3-dev
   pip install -r .devcontainer/requirements.txt
 
   # Let's have a user version of python3.
-  cp /usr/local/bin/python3.10 /home/vscode/.local/bin
-  ln -s /home/vscode/.local/bin/python3.10  /home/vscode/.local/bin/python3
+  # cp /usr/local/bin/python3.10 /home/vscode/.local/bin
+  # ln -s /home/vscode/.local/bin/python3.10  /home/vscode/.local/bin/python3
 
   # Version 8 needed for RISE slides. Generates a red compatibility error. 
   # pip install -U ipywidgets==8.0.0
 
   # Put extra packages here
 
-fi
+# fi
 
 
 # Notes:
