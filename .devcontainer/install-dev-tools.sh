67cf5bbffd7e3f06f6468e0c62802158e018c81e
Brian Lovell <lovell.brian@gmail.com>
2026-07-20 01:26:27 +0000

    Refactor install-dev-tools.sh to comment out installation logic and improve clarity

--- .devcontainer/install-dev-tools.sh
+++ .devcontainer/install-dev-tools.sh
@@ -1,27 +1,19 @@
 #!/bin/env bash
-# Put any custom installs in this file
 
 # Put files in USER_FOLDER. Only do this once. 
 
-USER_FOLDER="/home/vscode/.local"
-echo -e "****************************************************\n"
-if [ ! -d "$USER_FOLDER" ]; then
-    echo "$USER_FOLDER Directory missing"
-else
-    echo "$USER_FOLDER Directory exists"
-fi
-echo -e "****************************************************\n"
-if [ ! -d "$USER_FOLDER" ]; then
-    echo
-    echo -e "Creating the /home/vscode/.local python respository\n" 
-  # echo -e "Installing python3.10 and pip\n"
-  # install python code to ~/.vscode/.local
-  pip install --upgrade pip
-  pip install -r .devcontainer/requirements.txt
-else
-  echo -e "The /home/vscode/.local python respository is already there\n" 
-  # pip install fastbook
-fi
+# USER_FOLDER="/home/vscode/.local"
+# if [ ! -d "$USER_FOLDER" ]; then
+#     echo
+#     echo -e "Creating the /home/vscode/.local python respository\n" 
+#   # echo -e "Installing python3.10 and pip\n"
+#   # install python code to ~/.vscode/.local
+#   pip install --upgrade pip
+#   pip install -r .devcontainer/requirements.txt
+# else
+#   echo -e "The /home/vscode/.local python respository is already there\n" 
+#   # pip install fastbook
+# fi
 
 
 # Notes:
