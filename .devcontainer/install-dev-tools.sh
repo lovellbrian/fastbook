4eb7281539195ddd2d4e756491c8f84b333c3808
Brian Lovell <lovell.brian@gmail.com>
2026-07-20 03:19:02 +0000

    Comment out Python installation logic in install-dev-tools.sh

--- .devcontainer/install-dev-tools.sh
+++ .devcontainer/install-dev-tools.sh
@@ -2,24 +2,6 @@
 
 # Put files in USER_FOLDER. Only do this once. 
 
-USER_FOLDER="/home/vscode/.local"
-if [ ! -d "$USER_FOLDER" ]; then
-  echo -e "*******************************************************\n"
-  echo -e "Creating the /home/vscode/.local python respository\n"
-  echo -e "*******************************************************\n" 
-  # install python code to ~/.vscode/.local
-  pip install --upgrade pip
-  # sudo apt-get install -y libcairo2-dev 
-  pip install --break-system-packages -r .devcontainer/requirements.txt
-else
-  echo -e "*******************************************************\n"
-  echo -e "The /home/vscode/.local python respository already exists\n"
-  echo -e "*******************************************************\n"
-fi
-# pip install fastbook
-# pip install dtreeviz
-
-
 # Notes:
 # Commands for presentation using jupyter notebook
 # cd slides
